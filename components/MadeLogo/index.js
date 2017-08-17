33807da90e2ed12c7b60b58af9a591eed9db2581
Richard Foster <richard@madetech.co.uk>
2017-08-17 18:44:54 +0100

    Add MadeLogo component

---
+++ components/MadeLogo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from './logo.png'
+
+export default class MadeLogo extends React.Component {
+  render () {
+    return (
+      <img src={logo} alt="Made Tech" />
+    )
+  }
+}
---
+++ components/MadeLogo/stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import MadeLogo from './'
+
+storiesOf('MadeLogo', module)
+  .add('default', () =>
+    <MadeLogo />
+  )
