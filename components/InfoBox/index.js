e5ff17068e2c6102fb5141dfc803ae9888c92b7d
Richard Foster <richard@madetech.co.uk>
2017-08-17 19:00:10 +0100

    Add InfoBox component

---
+++ components/InfoBox/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { node, string } from 'prop-types'
+
+export default class InfoBox extends React.Component {
+  static propTypes = {
+    children: node.isRequired,
+    message: string.isRequired,
+  }
+
+  render () {
+    return (
+      <div>
+        <style jsx>{`
+          .info-box {
+            font-family: Helvetica, sans-serif;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            width: 100%;
+          }
+
+          .main {
+            display: block;
+            padding: 0 10px;
+          }
+
+          .message {
+            background: #eee;
+            border-top: 1px solid #ccc;
+            color: #999;
+            padding: 10px;
+          }
+        `}</style>
+
+        <div className="info-box">
+          <div className="main">
+            {this.props.children}
+          </div>
+          <div className="message">
+            {this.props.message}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
