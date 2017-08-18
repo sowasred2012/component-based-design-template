11f312f2447b5e42df0b746aa3a7c2b32455b697
Scott Mason <scott@madetech.co.uk>
2017-08-18 12:08:50 +0100

    remove example components we dont need

--- components/MadeHeader/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import MadeHeader from './'
-
-storiesOf('MadeHeader', module)
-  .add('default', () =>
-    <MadeHeader text="My test header" />
-  )
